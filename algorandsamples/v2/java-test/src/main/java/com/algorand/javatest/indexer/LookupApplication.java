074a97d7d96963290403c17b72404a70cac75d3c
Russell Fustino <rfustino@hotmail.com>
2021-06-10 12:04:08 -0400

    update response should look like this

--- algorandsamples/v2/java-test/src/main/java/com/algorand/javatest/indexer/LookupApplication.java
+++ algorandsamples/v2/java-test/src/main/java/com/algorand/javatest/indexer/LookupApplication.java
@@ -37,6 +37,7 @@ public class LookupApplication {
         IndexerClient indexerClientInstance = (IndexerClient)ex.connectToNetwork();
         // Long application_id = Long.valueOf(2672020);
         Long application_id = Long.valueOf(15974179);   
+       // 12397001
         Response<ApplicationResponse> response = indexerClientInstance
             .lookupApplicationByID(application_id).execute(); 
         // Response<ApplicationResponse> response = indexerClientInstance.lookupApplicationByID(application_id).execute(headers, values);    
@@ -48,24 +49,123 @@ public class LookupApplication {
 
     }
  }
-
-// response information should look similar to this...
-//  Response Info:
-//  {
-//   "application": {
-//     "id": 22,
-//     "params": {
-//       "global-state": [],
-//       "creator": "GHFRLVOMKJNTJ4HY3P74ZR4CNE2PB7CYAUAJ6HVAVVDX7ZKEMLJX6AAF4M",
-//       "local-state-schema": {
-//         "num-uint": 0,
-//         "num-byte-slice": 0
-//       },
-//       "global-state-schema": {
-//         "num-uint": 0,
-//         "num-byte-slice": 0
+// response shuld look like this
+//  Response Info: {
+//     "application": {
+//       "deleted": false,
+//       "created-at-round": 14412247,
+//       "id": 15974179,
+//       "params": {
+//         "clear-state-program": "AiABASJD",
+//         "global-state": [
+//           {
+//             "value": {
+//               "bytes": "",
+//               "type": 2,
+//               "uint": 200
+//             },
+//             "key": "YXNzZXRfY29lZmZpY2llbnQ="
+//           },
+//           {
+//             "value": {
+//               "bytes": "",
+//               "type": 2,
+//               "uint": 13164495
+//             },
+//             "key": "YXNzZXRfaWQ="
+//           },
+//           {
+//             "value": {
+//               "bytes": "",
+//               "type": 2,
+//               "uint": 4294967316
+//             },
+//             "key": "b3B0aW9uX2ZpcnN0"
+//           },
+//           {
+//             "value": {
+//               "bytes": "",
+//               "type": 2,
+//               "uint": 4294967326
+//             },
+//             "key": "b3B0aW9uX3NlY29uZA=="
+//           },
+//           {
+//             "value": {
+//               "bytes": "v5WyR1XaHaNu97Sngrp+bcpo3BLYGZNcFgFUUXK59Lg=",
+//               "type": 1,
+//               "uint": 0
+//             },
+//             "key": "Q3JlYXRvcg=="
+//           },
+//           {
+//             "value": {
+//               "bytes": "bXVjaGRlY2lzaW9u",
+//               "type": 1,
+//               "uint": 0
+//             },
+//             "key": "TmFtZQ=="
+//           },
+//           {
+//             "value": {
+//               "bytes": "",
+//               "type": 2,
+//               "uint": 4294967296
+//             },
+//             "key": "b3B0aW9uX2Fub3RoZXJvbmU="
+//           },
+//           {
+//             "value": {
+//               "bytes": "",
+//               "type": 2,
+//               "uint": 4294967296
+//             },
+//             "key": "b3B0aW9uX2hleQ=="
+//           },
+//           {
+//             "value": {
+//               "bytes": "",
+//               "type": 2,
+//               "uint": 4294967296
+//             },
+//             "key": "b3B0aW9uX3RoaXJk"
+//           },
+//           {
+//             "value": {
+//               "bytes": "",
+//               "type": 2,
+//               "uint": 1623000921
+//             },
+//             "key": "dm90aW5nX2VuZF90aW1l"
+//           },
+//           {
+//             "value": {
+//               "bytes": "",
+//               "type": 2,
+//               "uint": 1622136921
+//             },
+//             "key": "dm90aW5nX3N0YXJ0X3RpbWU="
+//           },
+//           {
+//             "value": {
+//               "bytes": "",
+//               "type": 2,
+//               "uint": 4294967296
+//             },
+//             "key": "b3B0aW9uX3NpeA=="
+//           }
+//         ],
+//         "creator": "X6K3ER2V3IO2G3XXWSTYFOT6NXFGRXAS3AMZGXAWAFKFC4VZ6S4K4JLOFY",
+//         "local-state-schema": {
+//           "num-uint": 1,
+//           "num-byte-slice": 1
+//         },
+//         "approval-program": "AiAFAAUEAYCAgIAQJgwEdm90ZQthZGRfb3B0aW9ucwdDcmVhdG9yBE5hbWUIYXNzZXRfaWQRYXNzZXRfY29lZmZpY2llbnQRdm90aW5nX3N0YXJ0X3RpbWUPdm90aW5nX2VuZF90aW1lC05VTExfT1BUSU9OB29wdGlvbl8UUVZvdGVEZWNpc2lvbkNyZWRpdHMBLTEYIhJAACYxGSMSQADJMRkkEkAAxzEZJRJAAMU2GgAoEkAA6zYaACkSQAFcACoxAGcrNhoAZycENhoGF2cnBTYaBxdnJwY2GggXZycHNhoJF2cyBycGZA1BAAIiQzYaAScIE0AABSVDQgAJJwk2GgFQIQRnNhoCJwgTQAAFJUNCAAknCTYaAlAhBGc2GgMnCBNAAAUlQ0IACScJNhoDUCEEZzYaBCcIE0AABSVDQgAJJwk2GgRQIQRnNhoFJwgTQAAFJUNCAAknCTYaBVAhBGclQyVDQgExIkNCASwiQ0IBJzIHJwZkDUEAAiJDIicEZHAANQA1ATQAQAAFIkNCAAwiJwo0AScFZAtmJUNCAPkyBycGZAxBAAIiQzIHJwdkDUEAAiJDIicJNhoBUGU1AjUDNAJAAAUiQ0IASiInCiInCmI2GgIXNhoCFwsJZiInCmIiD0AABSJDQgAoNhoDJwsSQAARJwk2GgFQNAM2GgIXCGdCAA4nCTYaAVA0AzYaAhcJZyVDQgCAMgcnBmQNQQACIkM2GgEnCBNAAAUlQ0IACScJNhoBUCEEZzYaAicIE0AABSVDQgAJJwk2GgJQIQRnNhoDJwgTQAAFJUNCAAknCTYaA1AhBGc2GgQnCBNAAAUlQ0IACScJNhoEUCEEZzYaBScIE0AABSVDQgAJJwk2GgVQIQRnJUM=",
+//         "global-state-schema": {
+//           "num-uint": 10,
+//           "num-byte-slice": 3
+//         }
 //       }
-//     }
-//   },
-//   "current-round": 377
-// }
+//     },
+//     "current-round": 14694513
+//   }
